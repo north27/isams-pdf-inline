ca7f24a48a0b7ba6d3a977fab47d440255a84adf
JMathew <jyothish.mathew@north27.co.uk>
2020-12-15 15:17:23 +0000

    - Updated git ignore. - Added option to open pdf inline with "octet-stream". - Updated to new North27 logos and icons.

--- background.js
+++ background.js
@@ -1,25 +1,61 @@
+// var responseListener = function (details) {
+//     const headers = details.responseHeaders;
+
+//     const contentType = headers.find(element => element.name == "Content-Type");
+//     alert(JSON.stringify(contentType));
+//     if ((contentType && contentType.value) && (contentType.value == 'application/pdf')) {
+//         alert(JSON.stringify(headers));
+//         var rule = {
+//             "name": "Content-Disposition",
+//             "value": "inline"
+//         };
+//         details.responseHeaders.push(rule);
+//         return { responseHeaders: details.responseHeaders };
+//     }
+// };
+
+// chrome.webRequest.onHeadersReceived.addListener(responseListener,
+//     {
+//         urls: [
+//             "*://*.isams.co.uk/*",
+//             "*://*.isams.cloud/*",
+//             "*://*.halcyonschool.com/*"
+//         ]
+//     },
+//     [
+//         "blocking",
+//         "responseHeaders"
+//     ]
+// );
+
+
 chrome.webRequest.onHeadersReceived.addListener((details) => {
 
     var headers = details.responseHeaders;
     var i = findInHeaders(headers, "content-disposition");
-    
+    console.log('headers', headers);
     if (i > -1) {
-
+        const cdValue = headers[i].value;
         let cd = headers[i].value.toLowerCase();
-        
-        if (cd.startsWith('attachment') && (cd.endsWith('.pdf"') || cd.endsWith('.pdf'))) {
+        const contentType = headers.find(element => element.name == "Content-Type");
+        const allowedTypes = [
+            'application/pdf',
+            'application/octet-stream; Charset=UTF-8'
+        ];
 
+        if (cd.startsWith('attachment') && (allowedTypes.find(element => element == contentType.value))) {
+            console.log('Inlining...');
             headers[i].value = 'inline' + headers[i].value.substr(10);
 
             var iContentType = findInHeaders(headers, "content-type");
-            headers[iContentType].value = 'application/pdf';
+            headers[iContentType].value = contentType.value;
             return {responseHeaders: headers};
         }
-        
+
     }
 
 },
-{urls: ["*://*.isams.co.uk/*","*://*.isams.cloud/*"]},
+{urls: ["*://*.isams.co.uk/*","*://*.isams.cloud/*","*://*.halcyonschool.com/*"]},
 ["responseHeaders","blocking"]);
 
 function findInHeaders(headers, name) {
